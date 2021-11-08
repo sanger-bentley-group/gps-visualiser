8131444e7d3be4a290073cdf7272d305e4036841
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2021-11-08 17:19:18 +0800

    Partial fix on country selection. Greenland is still broken.

--- front-end/scripts/main.js
+++ front-end/scripts/main.js
@@ -8,8 +8,16 @@ window.onload = function(){
     closeBtn.addEventListener('click', closeModal);
     overlay.addEventListener('click', closeModal);
 
-    function showModal(e) { 
+    // world-map.svg structure is different for some countries, therefore needing two checks. != #svg2985 to avoid ocean target
+    function showModal(e) {
+        let land = false;
         if (e.target.classList.contains("landxx")) {
+            land = true;
+        } else if (e.target['id'] != 'svg2985' && e.target.parentNode && e.target.parentNode.classList.contains("landxx")) {
+            land = true;
+        }
+
+        if (land) {
             overlay.classList.remove('modal-hidden');
             modal.classList.remove('modal-hidden');
         }
