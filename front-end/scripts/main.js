56654a68880cbb28e1e47c0d87fd07ef2cb4ad55
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2021-11-18 23:29:00 +0800

    Complete countrySelection function and improve its UI

--- front-end/scripts/main.js
+++ front-end/scripts/main.js
@@ -1,3 +1,7 @@
+// 'countries' is a const from countries.js exported by backend, contains alpha-2 codes of all countries with available data
+// 'alpha2Country' is a const in the alpha2.js, provides conversion from alpha-2 code to country name
+// 'countryAlpha2' is a generated const in the alpha2.js based on alpha2Country, provides conversion from country name to alpha-2 code
+
 // Delay script until all elemenets are loaded to ensure SVG is loaded
 window.onload = function(){
     let overlay = document.querySelector('#country-view-overlay');
@@ -6,8 +10,12 @@ window.onload = function(){
     let map = document.querySelector('#world-map').contentDocument;
     let countrySelector = document.querySelector('#country-selector');
 
+
+    // Initialise array to save state of country selection 
+    let countrySelection = countries.sort();
+
+
     // Add label and class to countires with available data
-    // 'countries' is a const from countries.js exported by backend, contains alpha-2 codes of all countries with available data
     countries.forEach(country => {
         let countryGroup = map.querySelector(`#${country}`);
 
@@ -17,10 +25,10 @@ window.onload = function(){
         // Add label group, enable the label and move it into the label group
         let countryLabel = map.querySelector(`#${country}-label`);
         let labelGroup = document.createElementNS("http://www.w3.org/2000/svg", "g");
-        labelGroup.setAttribute('id', `${country}-label-group`)
+        labelGroup.id = `${country}-label-group`;
         labelGroup.classList.add('country-label');
         countryLabel.before(labelGroup);
-        countryLabel.style.display = 'block'
+        countryLabel.style.display = 'block';
         labelGroup.append(countryLabel);
 
         // Add label background and move it into the label group
@@ -40,14 +48,15 @@ window.onload = function(){
         // Fill country-selector with flags based on countries.js
         // Flags from https://flagicons.lipis.dev/
         let flagElement = document.createElement('object');
-        flagElement.setAttribute('id', `${country}-flag`);
+        flagElement.id = `${country}-flag`;
         flagElement.classList.add('flag');
-        flagElement.setAttribute('type', 'image/svg+xml');
-        flagElement.setAttribute('data', `images/flags/${country}.svg`);
+        flagElement.type = 'image/svg+xml';
+        flagElement.data = `images/flags/${country}.svg`;
 
         let flagName = document.createTextNode(`${country}`);
 
         let flagDiv = document.createElement('div');
+        flagDiv.setAttribute('tooltip', `${alpha2Country[country]}`); // Custom arrtibute for tooltip support
         flagDiv.appendChild(flagElement);
         flagDiv.appendChild(flagName);
 
@@ -69,29 +78,65 @@ window.onload = function(){
         cbDiv.appendChild(flagLabel);
 
         countrySelector.appendChild(cbDiv);
+
+        // addEventListener to update countrySelection when checked/unchecked
+        flagInput.addEventListener('change', (e) => {
+            if (e.target.checked) {
+                countrySelection.push(e.target.value);
+                countrySelection.sort();
+            } else {
+                countrySelection = countrySelection.filter(item => item !== e.target.value);
+            }
+
+            // Tmp code for testing
+            document.querySelector('#tmp-output').innerHTML = countrySelection;
+        })
+
+        // addEventListener to highlight country in map when hovering its flag
+        cbDiv.addEventListener('mouseover', hoverFlag);
+        cbDiv.addEventListener('mouseout', unhoverFlag);
     }) 
 
 
-    // Save state of selection 
-    let countrySelection = countries
+    // Functions for hovering flag to highlight country in map
+    function hoverFlag(e) {
+        let selectedCountry = e.target.childNodes[0].id.slice(0, 2);
+        map.querySelector(`#${selectedCountry}`).classList.add('country-label-active')
+        map.querySelector(`#${selectedCountry}-label-group`).classList.add('country-label-active');
+        map.querySelector(`#${selectedCountry}-label-group`).firstChild.classList.add('country-label-bg-active');
+    }
 
+    function unhoverFlag(e) {
+        let selectedCountry = e.target.childNodes[0].id.slice(0, 2);
+        map.querySelector(`#${selectedCountry}`).classList.remove('country-label-active')
+        map.querySelector(`#${selectedCountry}-label-group`).classList.remove('country-label-active');
+        map.querySelector(`#${selectedCountry}-label-group`).firstChild.classList.remove('country-label-bg-active');
+    }
+
+
+    // addEventListener for select all and deselect all buttons
     document.querySelector('#sidebar-select-all').addEventListener('click', () => {
         countrySelection = countries;
         
         let flagCBs = document.querySelectorAll('.flag-cb');
-        console.log(countrySelection)
         flagCBs.forEach(flagCB => {
             flagCB.checked = true;
         })
+
+        // Tmp code for testing
+        document.querySelector('#tmp-output').innerHTML = countrySelection;
     })
+
     document.querySelector('#sidebar-deselect-all').addEventListener('click', () => {
         countrySelection = [];
 
         let flagCBs = document.querySelectorAll('.flag-cb');
-        console.log(countrySelection)
         flagCBs.forEach(flagCB => {
             flagCB.checked = false;
         })
+
+        // Tmp code for testing
+        document.querySelector('#tmp-output').innerHTML = countrySelection;
     })
 
     
@@ -106,8 +151,6 @@ window.onload = function(){
         // Limit response to countries with available data
         if (countries.indexOf(selectedCountry) !== -1) {
             let countryViewTitle = document.querySelector('#country-view-title');
-
-            // alpha2Country is a const in the alpha2.js
             countryViewTitle.innerHTML = `<h1>${alpha2Country[selectedCountry]}</h1>`;
             overlay.classList.remove('hidden');
             modal.classList.remove('hidden');
@@ -162,6 +205,3 @@ window.onload = function(){
     }
 
 }
-
-
-
