b04e5ed852a0ed88221a17360fcc0a2406541c42
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2021-11-26 23:23:10 +0800

    Working Icicle Protoype with Interlocking Update Charts

--- d3_prototype/icicle.js
+++ d3_prototype/icicle.js
@@ -1,28 +1,90 @@
 // Convert from https://observablehq.com/@kerryrodden/sequences-icicle
 
-// https://stackoverflow.com/questions/52861971/how-to-link-multiple-graph-networks-in-d3js-so-that-an-event-in-one-calls-the-sa
-// https://stackoverflow.com/questions/35090256/mouseover-event-on-two-charts-at-the-same-time-d3-js
-
 
 go();
 
-let outputPath = document.querySelector('#output')
-let outputPre = document.querySelector('#AU-precentage')
-let outputAbs = document.querySelector('#AU-absolute')
-
 async function go(){
-    await drawIcicle('sequence.csv', '#chart2');
-    await drawIcicle('sequence.csv', '#chart3');
+    await drawIcicle('AU.csv', '#chart2');
+    await drawIcicle('BR.csv', '#chart3');
 
-    document.querySelectorAll('.icicle').forEach(chart => {
-        console.log(chart.id);
-        let target = chart.querySelector('[data-path="Serotype_2_-_GPSC_4"]')
-        console.log(target.getAttribute('data-path'));
-        console.log(target.getAttribute('data-dValue'));
-        console.log(target.getAttribute('data-rValue'));
-        console.log('--------')
+    let charts = document.querySelectorAll('.icicle')
+
+    charts.forEach(chart => {
+        let chartId = chart.id;
+        let absolute = document.querySelector(`#${chartId}-absolute`);
+        let path = document.querySelector('#output');
+
+        let rValue = chart.childNodes[0].getAttribute('data-rValue');
+        absolute.innerHTML = `0 of ${rValue}`;
+
+        // Update chart visual, absolute, percentage and path output
+        chart.addEventListener('mouseover', (e) => {
+            if (e.target instanceof SVGRectElement) {
+                let dataPath = e.target.getAttribute('data-path');
+                path.innerHTML = dataPath.replaceAll('_', ' ').replaceAll('-', ' - ');
+
+                // Highlight same path in all charts, update absolute and percentage
+                charts.forEach(chart => {
+                    let chartId = chart.id;
+
+                    let percentage = document.querySelector(`#${chartId}-percentage`);
+                    let absolute = document.querySelector(`#${chartId}-absolute`);
+
+                    let rValue = chart.childNodes[0].getAttribute('data-rValue');
+                    
+                    let selectedPath = chart.querySelector(`[data-path='${dataPath}']`);
+                    if (selectedPath) {
+                        selectedPath.setAttribute('fill-opacity', '1.0');
+
+                        let dValue = selectedPath.getAttribute('data-dValue');
+                        percentage.innerHTML = `${((100 * dValue) / rValue).toPrecision(3)}%`
+                        absolute.innerHTML = `${dValue} of ${rValue}`;
+
+                        // If path is child, highlight parent as well
+                        let pathArray = dataPath.split('-');
+                        if (pathArray.length === 2) {
+                            let selectedParent = chart.querySelector(`[data-path='${pathArray[0]}']`);
+                            selectedParent.setAttribute('fill-opacity', '1.0');
+                        }
+                    } else {
+                        percentage.innerHTML = '0%';
+                        absolute.innerHTML = `0 of ${rValue}`;
+                    }
+                    
+                });
+            }
+        });
+
+        // Reset chart absolute, percentage and path output
+        chart.addEventListener('mouseout', (e) => {
+            let dataPath = e.target.getAttribute('data-path');
+
+            // Reset same path in all charts, reset absolute and percentage
+            charts.forEach(chart => {
+                let chartId = chart.id;
+
+                let percentage = document.querySelector(`#${chartId}-percentage`);
+                let absolute = document.querySelector(`#${chartId}-absolute`);
+
+                let rValue = chart.childNodes[0].getAttribute('data-rValue');
+                
+                percentage.innerHTML = '0%';
+                absolute.innerHTML = `0 of ${rValue}`;
+
+                let selectedPath = chart.querySelector(`[data-path='${dataPath}']`);
+                    if (selectedPath) {
+                        selectedPath.setAttribute('fill-opacity', '0.3');
+
+                        // If path is child, reset parent as well
+                        let pathArray = dataPath.split('-');
+                        if (pathArray.length === 2) {
+                            let selectedParent = chart.querySelector(`[data-path='${pathArray[0]}']`);
+                            selectedParent.setAttribute('fill-opacity', '0.3');
+                        }
+                    }
+            });
+        })
     })
-    
 };
 
 
@@ -46,11 +108,11 @@ async function drawIcicle(src, target) {
     );
 
     // Set color here, can be set to name specific color
-    // color = d3
-    //   .scaleOrdinal()
-    //   .domain(["home", "product", "search", "account", "other", "end"])
-    //   .range(["#5d85cf", "#7c6561", "#da7847", "#6fb971", "#9e70cf", "#bbbbbb"])
-    let color = d3.scaleOrdinal(d3.schemeTableau10);
+    // Arrays to be dynamically generated by back-end
+    let color = d3
+        .scaleOrdinal()
+        .domain(['Serotype 1', 'Serotype 2', 'Serotype 3', 'Serotype 4', 'GPSC 1', 'GPSC 2', 'GPSC 3', 'GPSC 4', 'GPSC 5'])
+        .range(['#ff0000', '#ffac00', '#f8ff00', '#5bff00', '#43d871', '#43b6d8', '#5443d8', '#b043d8', '#793865']);
 
     let segmentX = d => (d.x0);
     let segmentY = d => (d.y0);
@@ -59,36 +121,34 @@ async function drawIcicle(src, target) {
 
     const chart = await (() => {
         const root = partition(hierarchyData);
-        const svg = d3.create("svg");
-
-        outputAbs.innerHTML = `0 of ${root.value}`;
-
+        const svg = d3.create('svg');
         const element = svg.node(); 
 
         svg
-            .attr("viewBox", `0 0 ${width} ${height}`)
-            .style("max-width", `${width}px`)
-            .style("font", "12px sans-serif");
+            .attr('viewBox', `0 0 ${width} ${height}`)
+            .style('max-width', `${width}px`)
+            .style('font', '12px sans-serif')
+            .attr('data-rValue', `${root.value}`);
 
         const segment = svg
-            .append("g")
-            .attr("transform", `translate(0, ${-root.y1 / 2})`)
-            .selectAll("rect")
+            .append('g')
+            .attr('transform', `translate(0, ${-root.y1 / 2})`)
+            .selectAll('rect')
             .data(
               root.descendants().filter(d => {
                 // Don't draw the root node, and for efficiency, filter out nodes that would be too small to see
                 return d.depth && segmentWidth(d) >= 0.1;
               })
             )
-            .join("rect")
-            .attr("fill", d => color(d.data.name))
-            .attr("fill-opacity", 0.3)
-            .attr("x", segmentX)
-            .attr("y", segmentY)
-            .attr("width", segmentWidth)
-            .attr("height", segmentHeight)
-            .attr("class", "test")
-            .attr("data-path", d => {
+            .join('rect')
+            .attr('fill', d => color(d.data.name))
+            .attr('fill-opacity', 0.3)
+            .attr('x', segmentX)
+            .attr('y', segmentY)
+            .attr('width', segmentWidth)
+            .attr('height', segmentHeight)
+            .attr('class', 'test')
+            .attr('data-path', d => {
                 const sequence = d
                     .ancestors()
                     .reverse()
@@ -98,46 +158,18 @@ async function drawIcicle(src, target) {
                     sequence.forEach(node => {
                         output.push(node.data.name)
                     });
-                return `${output.join(' - ').replaceAll(' ','_')}`;
+                return `${output.join('-').replaceAll(' ','_')}`;
             })
-            .attr("data-dValue", d => `${d.value}`)
-            .attr("data-rValue", `${root.value}`)
-            .on("mouseleave", () => {
-                segment.attr("fill-opacity", 0.3);
-
-                outputPath.innerHTML = 'None';
-                outputPre.innerHTML = `0%`;
-                outputAbs.innerHTML = `0 of ${root.value}`;
-            })
-            .on("mouseenter", (event, d) => {
-                // Get the ancestors of the current segment, minus the root
-                const sequence = d
-                    .ancestors()
-                    .reverse()
-                    .slice(1);
-                // Highlight the ancestors
-                segment.attr("fill-opacity", node =>
-                    sequence.indexOf(node) >= 0 ? 1.0 : 0.3
-                );
-                const percentage = ((100 * d.value) / root.value).toPrecision(3);
-
-                output = [];
-                sequence.forEach(node => {
-                    output.push(node.data.name)
-                });
-                outputPath.innerHTML = `${output.join(' - ')}`;
-
-                outputPre.innerHTML = `${percentage}%`;
-                outputAbs.innerHTML = `${d.value} of ${root.value}`;
-            })
+            .attr('data-dValue', d => `${d.value}`)
         return element;
     })();
     document.querySelector(target).appendChild(chart);
 }
 
+
+// Helper function that transforms the given CSV into a hierarchical format.
 function buildHierarchy(csv) {
-    // Helper function that transforms the given CSV into a hierarchical format.
-    const root = { name: "root", children: [] };
+    const root = { name: 'root', children: [] };
     for (let i = 0; i < csv.length; i++) {
         const sequence = csv[i][0];
         const size = +csv[i][1];
@@ -145,17 +177,17 @@ function buildHierarchy(csv) {
             // e.g. if this is a header row
             continue;
         }
-        const parts = sequence.split("-");
+        const parts = sequence.split('-');
         let currentNode = root;
         for (let j = 0; j < parts.length; j++) {
-            const children = currentNode["children"];
+            const children = currentNode['children'];
             const nodeName = parts[j];
             let childNode = null;
             if (j + 1 < parts.length) {
             // Not yet at the end of the sequence; move down the tree.
             let foundChild = false;
             for (let k = 0; k < children.length; k++) {
-                if (children[k]["name"] == nodeName) {
+                if (children[k]['name'] == nodeName) {
                 childNode = children[k];
                 foundChild = true;
                 break;
