bbb619a7beeb3d88a0cfa8862a74328d7a44bdf9
Harry Hung <me@harryhung.com>
2021-12-28 16:16:27 +0800

    Prep work for antibiotic resistance bar chart prototype

---
+++ d3_prototype/barchart.js
@@ -0,0 +1,11 @@
+go();
+
+async function go(){
+    const data = await (await fetch('data.json')).json();
+    await drawBarChart(data['country']['AR']['resistance']['age0'], '#chart5');
+    await drawBarChart(data['country']['AR']['resistance']['age1'], '#chart6');
+}
+
+async function drawBarChart(data, target) {
+    console.log(data)
+}
